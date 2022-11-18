2fe3d243a376cd532b84b2ee069f0f11336e7ae1
Jerome <101889450+jeromealmir@users.noreply.github.com>
2022-11-18 01:53:25 -0800

    change key actions from print command strings to send command to server using connection

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = () => {
+let connection;
+
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -8,10 +11,29 @@ const setupInput = () => {
 };
 
 const handleUserInput = (key) => {
+
   if (key === '\u0003') {
     console.log('Thank you for playing!');
     process.exit();
   }
+
+  if (key === 'w') {
+    connection.write('Move: up');
+  }
+
+  if (key === 'a') {
+    connection.write('Move: left');
+  }
+
+  if (key === 's') {
+    connection.write('Move: down');
+  }
+
+  if (key === 'd') {
+    connection.write('Move: right');
+  }
+
 };
+
 
 module.exports = setupInput;
