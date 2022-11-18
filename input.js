3d24fc49b437df99397d0ff0b9ea8424a46888a4
Jerome <101889450+jeromealmir@users.noreply.github.com>
2022-11-17 21:53:03 -0800

    move setupInput and handleUserInput function and export setupInput to module

---
+++ input.js
@@ -0,0 +1,17 @@
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume(); // <= stream is paused by default so stdin.resume() needs to be called to read from it!
+  stdin.on('data', handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = (key) => {
+  if (key === '\u0003') {
+    console.log('Thank you for playing!');
+    process.exit();
+  }
+};
+
+module.exports = setupInput;
