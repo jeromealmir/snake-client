570b002fb3602b49bc0440ee116207e02dcd6d42
Jerome <101889450+jeromealmir@users.noreply.github.com>
2022-11-18 02:28:53 -0800

    create a constant file where all constant information are stored for modularity

---
+++ constant.js
@@ -0,0 +1,11 @@
+const IP = '10.0.0.243';
+const PORT = '50541';
+const keys = {
+  w: 'Move: up',
+  a: 'Move: left',
+  s: 'Move: down',
+  d: 'Move: right',
+  g: 'Say: Let\'s go!'
+};
+
+module.exports = {IP, PORT, keys};
