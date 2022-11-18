dd666eae604522793f974bb20de5271c293a9369
Jerome <101889450+jeromealmir@users.noreply.github.com>
2022-11-17 21:32:46 -0800

    create setupInput and handleUserInput function to listen for keyboard input

--- play.js
+++ play.js
@@ -1,4 +1,23 @@
 const connect = require('./client');
 
 console.log('Connecting...');
+
 connect();
+
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume(); // <= stream is paused by default so stdin.resume() needs to be called to read from it!
+  stdin.on('data', handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = (key) => {
+  if (key === '\u0003') {
+    console.log('Thank you for playing!');
+    process.exit();
+  }
+};
+
+setupInput();
