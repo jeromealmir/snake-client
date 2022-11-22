5bb6a8096b96e6db2f45a1993084c4c0d43f5dcc
Jerome <101889450+jeromealmir@users.noreply.github.com>
2022-11-22 14:22:24 -0800

    add comment and few more canned messages

--- constant.js
+++ constant.js
@@ -1,3 +1,5 @@
+//file contains all constant variable in the game
+
 const IP = '10.0.0.243';
 const PORT = '50541';
 const keys = {
@@ -5,7 +7,9 @@ const keys = {
   a: 'Move: left',
   s: 'Move: down',
   d: 'Move: right',
-  g: 'Say: Let\'s go!'
+  g: 'Say: Let\'s go!',
+  h: 'Say: Howdy!',
+  b: 'Say: Goodbye!'
 };
 
 module.exports = {IP, PORT, keys};
