e9378e5e23c16ebf5714cf81faaf6ee734dfec03
Jerome <101889450+jeromealmir@users.noreply.github.com>
2022-11-22 14:23:56 -0800

    add comments

--- client.js
+++ client.js
@@ -1,19 +1,29 @@
 const net = require('net');
+
+//import IP and PORT number from constant file
 const {IP, PORT} = require('./constant');
 
 //establishes a connection with the game server
 const connect = () => {
   const conn =  net.createConnection({
-    host: IP, // IP address here,
-    port: PORT //PORT number here,
+    host: IP,
+    port: PORT
   });
 
-  conn.on('data', (data) => {
-    console.log(data);
-  });
-
+  //event listener that logs a message when connection is successfully established
   conn.on('connect', () => {
     console.log('Successfully connected to game server.');
+  });
+
+  //receive and log data sent by server
+  conn.on('data', (data) => {
+    console.log(`Server says: ${data}`);
+  });
+
+  //event listener that logs a message when connection is terminated
+  conn.on('end', () => {
+    console.log('Game Over!');
+    process.exit();
   });
 
   //interpret incoming data as text
