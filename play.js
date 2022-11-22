9a4c5052bd61474cb5e694b92daa3eabbcf3fca0
Jerome <101889450+jeromealmir@users.noreply.github.com>
2022-11-22 14:23:10 -0800

    add comments

--- play.js
+++ play.js
@@ -1,6 +1,8 @@
+//import connect and setupInput function
 const connect = require('./client');
 const setupInput = require('./input');
 
 console.log('Connecting...');
 
+//connect() is passed as variable here so setupInput can use conn object by connect function
 setupInput(connect());
