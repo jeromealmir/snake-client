c82bc3095fe65f831291ae10d829836b4ee48652
Jerome <101889450+jeromealmir@users.noreply.github.com>
2022-11-22 14:23:01 -0800

    add comments and add Q as command to quit game

--- input.js
+++ input.js
@@ -1,26 +1,38 @@
+//WASD movement keys import from constant
 const { keys } = require('./constant');
+
 const commandLine = process.argv.slice(2);
 
+//represents the conn object
 let connection;
 
 const setupInput = (conn) => {
+
   connection = conn;
+
+  //stdin handles user input
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
-  stdin.resume(); // <= stream is paused by default so stdin.resume() needs to be called to read from it!
+  stdin.resume();
   stdin.on('data', handleUserInput);
+
+  //set snake initials from command line
   connection.write(`Name: ${commandLine}`);
+
   return stdin;
 };
 
 const handleUserInput = (key) => {
 
+  //loop through keys object and send command to server
   for (const k in keys) {
     if (k === key) {
       connection.write(keys[k]);
     }
-    if (key === '\u0003') {
+
+    //terminate the game by pressing ctrl + c
+    if (key === '\u0003' || key === 'q') {
       console.log('Thank you for playing!');
       process.exit();
     }
